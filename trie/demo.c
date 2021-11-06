435dd90cb013303a3bf3a1afd6dce055307a8ae4
LukaFMF <luka.toplak@student.fmf.uni-lj.si>
2021-11-06 04:51:46 +0100

    Finished trie

--- trie/demo.c
+++ trie/demo.c
@@ -1,13 +1,79 @@
-
-
+#include <stdio.h>
+#include <stdlib.h>
 #include "trie.h"
+
+
+bool verify_word(const char *str)
+{
+	const u32 len = strlen(str);
+	if(len == 0u || str[0] == '-')
+		return false;
+	
+	for(u32 i = 0u;i < len;i++)
+		if(str[i] >= 'a' && str[i] <= 'z')
+			return true;
+
+	return false;
+}
+
+void fill_trie(trie *t,const char *filename)
+{
+	FILE *file = fopen(filename,"r");
+	fseek(file,0u,SEEK_END);
+	const u64 file_size = ftell(file);
+	rewind(file);
+
+	char *file_contents = malloc(file_size);
+	fread(file_contents,file_size,1u,file);
+	fclose(file);
+
+	char *str_start = file_contents;
+	for(u32 i = 0u;i < file_size;i++)
+	{
+		const char curr_char = file_contents[i];
+		switch(curr_char)
+		{
+			case ' ':
+			case ',':
+			case '.':
+			case '?':
+			case '!':
+			case ':':
+			case ';':
+			case '(':
+			case ')':
+			case '\n':
+			case '\"':
+			{
+				file_contents[i] = '\0';
+				if(verify_word(str_start))
+				{
+					//printf("%s -- ",str_start);
+					trie_insert(t,str_start);
+					//printf("%s\n",str_start);
+				}
+				str_start = file_contents + i + 1;
+				break;
+			}
+		}
+
+		// convert to lowercase
+		if(curr_char >= 'A' && curr_char <= 'Z')
+			file_contents[i] += 32;
+	}
+	free(file_contents);
+}
 
 i32 main(void)
 {
 	trie t;
+
 	trie_init(&t);
 
-	trie_inset(&t,"abc");
+	fill_trie(&t,"TheSorcerersStone.txt");
+	trie_write(&t,"TheSorcerersStoneWords.txt");
 
+	trie_cleanup(&t);
+	
 	return 0;
 }
--- trie/trie.h
+++ trie/trie.h
@@ -7,7 +7,7 @@
 typedef struct trie_node
 {
 	char character;
-	bool end_of_word;
+	bool word_mark;
 	u32 num_next_nodes;
 	struct trie_node **next_nodes;
 } trie_node;
@@ -15,21 +15,24 @@ typedef struct trie_node
 
 typedef struct trie
 {
-	u32 max_depth;
+	u32 size;
+	u32 max_str_len;
 	trie_node *empty_str;
 } trie;
 
 void trie_init(trie *t)
 {
-	t->max_depth = 0u;
+	t->size = 0u;
+	t->max_str_len = 0u; 
 	t->empty_str = malloc(sizeof(trie_node));
 	t->empty_str->character = '\0';
-	t->empty_str->end_of_word = false;
+	t->empty_str->word_mark = false;
 	t->empty_str->num_next_nodes = 0u;
 }
 
-void trie_inset(trie *t,const char *str)
+void trie_insert(trie *t,const char *str)
 {
+	//printf("INSERTING: %s\n",str);
 	trie_node *curr_node = t->empty_str;
 
 	u32 curr_depth = 0u;
@@ -37,39 +40,47 @@ void trie_inset(trie *t,const char *str)
 	{
 		u32 insert_inx = 0u;
 		bool found = false;
-		char curr_char = str[i];
+		bool insert_to_last = true;
+		const char curr_char = str[i];
 		for(u32 j = 0u;j < curr_node->num_next_nodes;j++)
 		{
-			char possible_next_char = curr_node->next_nodes[j]->character;
+			const char possible_next_char = curr_node->next_nodes[j]->character;
 			if(curr_char == possible_next_char)
 			{
 				found = true;
-				break;
-			}
-			else if(j == 0u && curr_char < possible_next_char)
-			{
 				insert_inx = j;
 				break;
 			}
-			else if(curr_char > curr_node->next_nodes[j-1]->character && curr_char < possible_next_char)
+			else if(curr_char < possible_next_char)
 			{
+				insert_to_last = false;
 				insert_inx = j;
 				break;
 			}
-
 		}
+
 		// if character has no barnch yet we create it
 		if(!found)
 		{
 			if(curr_node->num_next_nodes == 0u)
 			{
+				//printf("first insert %c\n",curr_char);
 				curr_node->num_next_nodes = 1u;
 				curr_node->next_nodes = malloc(sizeof(trie_node*));
 				curr_node->next_nodes[0] = malloc(sizeof(trie_node));
 			}
+			else if(insert_to_last)
+			{
+				//printf("insert to last: %c\n",curr_char);
+				curr_node->num_next_nodes++;
+				curr_node->next_nodes = realloc(curr_node->next_nodes,curr_node->num_next_nodes*sizeof(trie_node*));
+
+				insert_inx = curr_node->num_next_nodes - 1;
+				curr_node->next_nodes[insert_inx] = malloc(sizeof(trie_node));
+			}
 			else
 			{
-				// TODO alloc space at pointers also, not just pointer array !!!!!!!!!!!!
+				//printf("insert to %d: %c\n",insert_inx,curr_char);
 				curr_node->num_next_nodes++;
 				curr_node->next_nodes = realloc(curr_node->next_nodes,curr_node->num_next_nodes*sizeof(trie_node*));
 
@@ -77,18 +88,69 @@ void trie_inset(trie *t,const char *str)
 				trie_node **move_src = curr_node->next_nodes + insert_inx;
 				const u32 bytes_to_move = (curr_node->num_next_nodes - insert_inx - 1)*sizeof(trie_node*);
 				memmove(move_src + 1,move_src,bytes_to_move);
+				curr_node->next_nodes[insert_inx] = malloc(sizeof(trie_node));
 			}
 			trie_node *next_node = curr_node->next_nodes[insert_inx];
 			next_node->character = curr_char;
-			next_node->end_of_word = false;
+			next_node->word_mark = false;
 			next_node->num_next_nodes = 0u;
 		}
 		curr_node = curr_node->next_nodes[insert_inx];
+		curr_depth++;
 	}
-	curr_node->end_of_word = true;
+
+	// if word already exists in trie
+	if(!curr_node->word_mark)
+	{
+		curr_node->word_mark = true;
+		t->size++;
+	}
+
+	if(curr_depth > t->max_str_len)
+		t->max_str_len = curr_depth;
 }
+
+void trie_cleanup_rec(trie_node *curr)
+{
+	for(u32 i = 0u;i < curr->num_next_nodes;i++)
+	{
+		trie_cleanup_rec(curr->next_nodes[i]);
+		free(curr->next_nodes[i]);
+	}
+	if(curr->num_next_nodes > 0)
+		free(curr->next_nodes);
+	free(curr);
+} 
 
 void trie_cleanup(trie *t)
 {
-	
+	trie_cleanup_rec(t->empty_str);
+	t->size = 0u;
+	t->max_str_len = 0u; 
+}
+
+void trie_write_rec(trie_node *curr,char *str,u32 depth,FILE *file)
+{
+	//printf("%c - %s\n",curr->character,curr->word_mark ? "true" : "false");
+	if(depth > 0)
+		str[depth-1] = curr->character;
+
+	if(curr->word_mark)
+	{
+		str[depth] = '\0';
+		fprintf(file,"%s\n",str);
+	}
+
+	for(u32 i = 0u;i < curr->num_next_nodes;i++)
+	{
+		trie_write_rec(curr->next_nodes[i],str,depth + 1,file);
+	}
+}
+
+void trie_write(trie *t,const char *filename)
+{
+	char *s = malloc(t->max_str_len + 1);
+	FILE *file = fopen(filename,"w");
+	trie_write_rec(t->empty_str,s,0u,file);
+	free(s);
 }
