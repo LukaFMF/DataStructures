ef44bb527283622c4bc596a7a098c69b3b2ee960
LukaFMF <luka.toplak@student.fmf.uni-lj.si>
2021-11-07 05:52:34 +0100

    More stuff

---
+++ dyn_array/demo.c
@@ -0,0 +1,30 @@
+#include "dyn_array.h"
+
+#include <time.h>
+
+def_dyn_array(i32,i32)
+
+void print_arr(const struct dyn_array_i32 *a)
+{
+	for(u32 i = 0u;i < a->size;i++)
+		printf("%d ",a->data[i]);
+	printf("-- %d\n",a->capacity);
+}
+
+i32 main(void)
+{
+	srand(time(0));
+	struct dyn_array_i32 a;
+	dyn_array_i32_init(&a);
+
+	for(u32 n = 0;n < 100;n++)
+	{
+		for(u32 i = 0;i < 100;i++)
+			dyn_array_i32_insert(&a,rand() % (a.size + 1),rand() % 100 + 1);
+
+		//print_arr(&a);
+		dyn_array_i32_reset(&a);
+	}
+
+	return 0;
+}
