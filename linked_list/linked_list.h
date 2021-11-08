08f3d4577a69c8772a5627c0863fe6b7d360f607
LukaFMF <luka.toplak@student.fmf.uni-lj.si>
2021-11-09 00:10:04 +0100

    Small fix

--- linked_list/linked_list.h
+++ linked_list/linked_list.h
@@ -4,26 +4,26 @@
 #include "../utils/types.h"
 
 #define DEF_LINKED_LIST(NAME,TYPE)											\
-typedef struct link_node_##NAME link_node_##NAME;											\
-typedef struct link_node_##NAME													\
+typedef struct link_node_##NAME link_node_##NAME;							\
+typedef struct link_node_##NAME												\
 {																			\
 	TYPE data;																\
-	link_node_##NAME *next;														\
-} link_node_##NAME;																\
+	link_node_##NAME *next;													\
+} link_node_##NAME;															\
 \
-typedef struct linked_list_##NAME													\
+typedef struct linked_list_##NAME											\
 {																			\
 	u32 size;																\
-	link_node_##NAME *head;														\
-} linked_list_##NAME;																\
+	link_node_##NAME *head;													\
+} linked_list_##NAME;														\
 \
-void linked_list_##NAME##_init(linked_list_##NAME *l)										\
+void linked_list_##NAME##_init(linked_list_##NAME *l)						\
 {																			\
 	l->size = 0u;															\
 	l->head = NULL;															\
 }																			\
 \
-void linked_list_##NAME##_insert(linked_list_##NAME *l,TYPE value,u32 index)					\
+void linked_list_##NAME##_insert(linked_list_##NAME *l,TYPE value,u32 index)\
 {																			\
 	if(index > l->size)														\
 	{																		\
@@ -34,24 +34,24 @@ void linked_list_##NAME##_insert(linked_list_##NAME *l,TYPE value,u32 index)				
 																			\
 	if(index == 0)															\
 	{																		\
-		link_node_##NAME *new = malloc(sizeof(link_node_##NAME));							\
+		link_node_##NAME *new = malloc(sizeof(link_node_##NAME));			\
 		new->data = value;													\
 																			\
-		link_node_##NAME *next = l->head;											\
+		link_node_##NAME *next = l->head;									\
 		l->head = new;														\
 		new->next = next;													\
 	}																		\
 	else																	\
 	{																		\
-		link_node_##NAME *prev = l->head;											\
-		link_node_##NAME *next = prev->next;										\
+		link_node_##NAME *prev = l->head;									\
+		link_node_##NAME *next = prev->next;								\
 		for(u32 i = 1u;i < index;i++)										\
 		{																	\
 			prev = next;													\
 			next = next->next;												\
 		}																	\
 																			\
-		link_node_##NAME *new = malloc(sizeof(link_node_##NAME));							\
+		link_node_##NAME *new = malloc(sizeof(link_node_##NAME));			\
 		new->data = value;													\
 																			\
 		prev->next = new;													\
@@ -60,7 +60,7 @@ void linked_list_##NAME##_insert(linked_list_##NAME *l,TYPE value,u32 index)				
 	l->size++;																\
 }																			\
 \
-void linked_list_##NAME##_delete(linked_list_##NAME *l,u32 index)							\
+void linked_list_##NAME##_delete(linked_list_##NAME *l,u32 index)			\
 {																			\
 	if(l->size == 0u || index > l->size - 1)								\
 	{																		\
@@ -71,14 +71,14 @@ void linked_list_##NAME##_delete(linked_list_##NAME *l,u32 index)							\
 																			\
 	if(index == 0)															\
 	{																		\
-		link_node_##NAME *to_delete = l->head;										\
+		link_node_##NAME *to_delete = l->head;								\
 		l->head = to_delete->next;											\
 		free(to_delete);													\
 	}																		\
 	else																	\
 	{																		\
-		link_node_##NAME *prev = l->head;											\
-		link_node_##NAME *to_delete = prev->next;									\
+		link_node_##NAME *prev = l->head;									\
+		link_node_##NAME *to_delete = prev->next;							\
 		for(u32 i = 1u;i < index;i++)										\
 		{																	\
 			prev = to_delete;												\
@@ -91,10 +91,10 @@ void linked_list_##NAME##_delete(linked_list_##NAME *l,u32 index)							\
 	l->size--;																\
 }																			\
 \
-void linked_list_##NAME##_cleanup(linked_list_##NAME *l)									\
+void linked_list_##NAME##_cleanup(linked_list_##NAME *l)					\
 {																			\
-	link_node_##NAME *curr = l->head;												\
-	link_node_##NAME *next = NULL;													\
+	link_node_##NAME *curr = l->head;										\
+	link_node_##NAME *next = NULL;											\
 	while(curr != NULL)														\
 	{																		\
 		next = curr->next;													\
@@ -106,8 +106,8 @@ void linked_list_##NAME##_cleanup(linked_list_##NAME *l)									\
 	l->size = 0u;															\
 }																			\
 \
-void linked_list_##NAME##_reset(linked_list_##NAME *l)										\
+void linked_list_##NAME##_reset(linked_list_##NAME *l)						\
 {																			\
-	linked_list_##NAME##_cleanup(l);													\
-	linked_list_##NAME##_init(l);													\
+	linked_list_##NAME##_cleanup(l);										\
+	linked_list_##NAME##_init(l);											\
 }
