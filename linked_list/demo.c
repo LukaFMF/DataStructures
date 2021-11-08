0c8f832c487b022af88782a2960b76a743aef01c
LukaFMF <luka.toplak@student.fmf.uni-lj.si>
2021-11-09 00:09:38 +0100

    Generic linked list

--- linked_list/demo.c
+++ linked_list/demo.c
@@ -1,23 +1,37 @@
 #include "linked_list.h"
 
+DEF_LINKED_LIST(i32,i32)
+
+void linked_list_i32_print(const linked_list_i32 l)
+{
+	printf("HEAD ");
+	link_node_i32 *curr = l.head;
+	while(curr != NULL)
+	{
+		printf("--> %d ",curr->data);
+		curr = curr->next;
+	}
+	printf("--> NULL\n");
+}
+
 i32 main(void)
 {
-	linked_list l;
-	linked_list_init(&l);
+	linked_list_i32 l;
+	linked_list_i32_init(&l);
 	printf("Empty linked list:\n");
-	linked_list_print(l);
+	linked_list_i32_print(l);
 
 	printf("Linked list with 3 elements:\n");
-	linked_list_insert(&l,3,0u);
-	linked_list_insert(&l,6,1u);
-	linked_list_insert(&l,9,2u);
-	linked_list_print(l);
+	linked_list_i32_insert(&l,3,0u);
+	linked_list_i32_insert(&l,6,1u);
+	linked_list_i32_insert(&l,9,2u);
+	linked_list_i32_print(l);
 
 	printf("Linked list after removing the second element:\n");
-	linked_list_delete(&l,1u);
-	linked_list_print(l);
+	linked_list_i32_delete(&l,1u);
+	linked_list_i32_print(l);
 
-	linked_list_cleanup(&l);
+	linked_list_i32_cleanup(&l);
 	// Benchmark
 	// for(u32 i = 0u;i < 1000;i++)
 	// {
--- linked_list/linked_list.h
+++ linked_list/linked_list.h
@@ -1,114 +1,113 @@
 #include <stdlib.h>
 #include <stdio.h>
+
 #include "../utils/types.h"
 
-typedef struct link_node
-{
-	i32 data;
-	struct link_node *next;
-} link_node;
-
-typedef struct linked_list
-{
-	link_node *head;
-	u32 size;
-} linked_list;
-
-void linked_list_init(linked_list *l)
-{
-	l->head = NULL;
-	l->size = 0u;
-}
-
-void linked_list_insert(linked_list *l,i32 value,u32 index)
-{
-	if(index > l->size)
-	{
-		printf("Cannot insert to index %d, because it exceeds size of the list. Exiting!",index);
-		exit(-1);
-	}
-	
-	if(index == 0)
-	{
-		link_node *new = malloc(sizeof(link_node));
-		new->data = value;
-
-		link_node *next = l->head;
-		l->head = new;
-		new->next = next;
-	}
-	else
-	{
-		link_node *prev = l->head;
-		link_node *next = prev->next;
-		for(u32 i = 1u;i < index;i++)
-		{
-			prev = next;
-			next = next->next;
-		}
-
-		link_node *new = malloc(sizeof(link_node));
-		new->data = value;
-
-		prev->next = new;
-		new->next = next;
-	}
-	l->size++;
-}
-
-void linked_list_delete(linked_list *l,u32 index)
-{
-	if(l->size == 0u || index > l->size - 1)
-	{
-		printf("Cannot delete from index %d, because it exceeds (size of the list) - 1. Exiting!",index);
-		exit(-1);
-	}
-
-	if(index == 0)
-	{
-		link_node *to_delete = l->head;
-		l->head = to_delete->next;
-		free(to_delete);
-	}
-	else
-	{
-		link_node *prev = l->head;
-		link_node *to_delete = prev->next;
-		for(u32 i = 1u;i < index;i++)
-		{
-			prev = to_delete;
-			to_delete = to_delete->next;
-		}
-
-		prev->next = to_delete->next;
-		free(to_delete);
-	}
-	l->size--;
-}
-
-void linked_list_cleanup(linked_list *l)
-{
-	link_node *curr = l->head;
-	link_node *next = NULL;
-	while(curr != NULL)
-	{
-		next = curr->next;
-		free(curr);
-		curr = next;
-	}
-
-	l->head = NULL;
-	l->size = 0u;
-}
-
-void linked_list_print(linked_list l)
-{
-	printf("HEAD ");
-	link_node *curr = l.head;
-	while(curr != NULL)
-	{
-		printf("--> %d ",curr->data);
-		curr = curr->next;
-	}
-	printf("--> NULL\n");
+#define DEF_LINKED_LIST(NAME,TYPE)											\
+typedef struct link_node_##NAME link_node_##NAME;											\
+typedef struct link_node_##NAME													\
+{																			\
+	TYPE data;																\
+	link_node_##NAME *next;														\
+} link_node_##NAME;																\
+\
+typedef struct linked_list_##NAME													\
+{																			\
+	u32 size;																\
+	link_node_##NAME *head;														\
+} linked_list_##NAME;																\
+\
+void linked_list_##NAME##_init(linked_list_##NAME *l)										\
+{																			\
+	l->size = 0u;															\
+	l->head = NULL;															\
+}																			\
+\
+void linked_list_##NAME##_insert(linked_list_##NAME *l,TYPE value,u32 index)					\
+{																			\
+	if(index > l->size)														\
+	{																		\
+		printf("Cannot insert to index %d, because it "						\
+		"exceeds size of the list. Exiting!",index);						\
+		exit(-1);															\
+	}																		\
+																			\
+	if(index == 0)															\
+	{																		\
+		link_node_##NAME *new = malloc(sizeof(link_node_##NAME));							\
+		new->data = value;													\
+																			\
+		link_node_##NAME *next = l->head;											\
+		l->head = new;														\
+		new->next = next;													\
+	}																		\
+	else																	\
+	{																		\
+		link_node_##NAME *prev = l->head;											\
+		link_node_##NAME *next = prev->next;										\
+		for(u32 i = 1u;i < index;i++)										\
+		{																	\
+			prev = next;													\
+			next = next->next;												\
+		}																	\
+																			\
+		link_node_##NAME *new = malloc(sizeof(link_node_##NAME));							\
+		new->data = value;													\
+																			\
+		prev->next = new;													\
+		new->next = next;													\
+	}																		\
+	l->size++;																\
+}																			\
+\
+void linked_list_##NAME##_delete(linked_list_##NAME *l,u32 index)							\
+{																			\
+	if(l->size == 0u || index > l->size - 1)								\
+	{																		\
+		printf("Cannot delete from index %d, because it "					\
+		"exceeds %d. Exiting!",index,l->size-1);							\
+		exit(-1);															\
+	}																		\
+																			\
+	if(index == 0)															\
+	{																		\
+		link_node_##NAME *to_delete = l->head;										\
+		l->head = to_delete->next;											\
+		free(to_delete);													\
+	}																		\
+	else																	\
+	{																		\
+		link_node_##NAME *prev = l->head;											\
+		link_node_##NAME *to_delete = prev->next;									\
+		for(u32 i = 1u;i < index;i++)										\
+		{																	\
+			prev = to_delete;												\
+			to_delete = to_delete->next;									\
+		}																	\
+																			\
+		prev->next = to_delete->next;										\
+		free(to_delete);													\
+	}																		\
+	l->size--;																\
+}																			\
+\
+void linked_list_##NAME##_cleanup(linked_list_##NAME *l)									\
+{																			\
+	link_node_##NAME *curr = l->head;												\
+	link_node_##NAME *next = NULL;													\
+	while(curr != NULL)														\
+	{																		\
+		next = curr->next;													\
+		free(curr);															\
+		curr = next;														\
+	}																		\
+																			\
+	l->head = NULL;															\
+	l->size = 0u;															\
+}																			\
+\
+void linked_list_##NAME##_reset(linked_list_##NAME *l)										\
+{																			\
+	linked_list_##NAME##_cleanup(l);													\
+	linked_list_##NAME##_init(l);													\
 }
